21b2597e9ffa451bd440fd71fcdfd155164d0b21
Joshua Lochner <admin@xenova.com>
2024-08-22 20:40:26 +0000

    Prettier format

--- bun/index.ts
+++ bun/index.ts
@@ -1,11 +1,14 @@
-import { pipeline } from '@huggingface/transformers';
+import { pipeline } from "@huggingface/transformers";
 
 // Create a feature-extraction pipeline
-const extractor = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
+const extractor = await pipeline(
+  "feature-extraction",
+  "Xenova/all-MiniLM-L6-v2",
+);
 
 // Compute sentence embeddings
-const sentences = ['Hello world', 'This is an example sentence'];
-const output = await extractor(sentences, { pooling: 'mean', normalize: true });
+const sentences = ["Hello world", "This is an example sentence"];
+const output = await extractor(sentences, { pooling: "mean", normalize: true });
 console.log(output.tolist());
 // [
 //   [ -0.03172111138701439, 0.04395204409956932, 0.00014728980022482574, ... ],
--- pglite-semantic-search/src/App.jsx
+++ pglite-semantic-search/src/App.jsx
@@ -1,9 +1,9 @@
-import { getDB, initSchema, countRows, seedDb, search } from './utils/db';
-import { useState, useEffect, useRef, useCallback } from 'react';
+import { getDB, initSchema, countRows, seedDb, search } from "./utils/db";
+import { useState, useEffect, useRef, useCallback } from "react";
 
 export default function App() {
   // Keep track of the classification result and the model loading status.
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [content, setContent] = useState([]);
   const [result, setResult] = useState(null);
   const [ready, setReady] = useState(null);
@@ -19,15 +19,15 @@ export default function App() {
       initailizing.current = true;
       db.current = await getDB();
       await initSchema(db.current);
-      let count = await countRows(db.current, 'embeddings');
+      let count = await countRows(db.current, "embeddings");
       console.log(`Found ${count} rows`);
       if (count === 0) {
         await seedDb(db.current);
-        count = await countRows(db.current, 'embeddings');
+        count = await countRows(db.current, "embeddings");
         console.log(`Seeded ${count} rows`);
       }
       // Get Items
-      const items = await db.current.query('SELECT content FROM embeddings');
+      const items = await db.current.query("SELECT content FROM embeddings");
       setContent(items.rows.map((x) => x.content));
     };
     if (!db.current && !initailizing.current) {
@@ -39,21 +39,21 @@ export default function App() {
   useEffect(() => {
     if (!worker.current) {
       // Create the worker if it does not yet exist.
-      worker.current = new Worker(new URL('./worker.js', import.meta.url), {
-        type: 'module',
+      worker.current = new Worker(new URL("./worker.js", import.meta.url), {
+        type: "module",
       });
     }
 
     // Create a callback function for messages from the worker thread.
     const onMessageReceived = async (e) => {
       switch (e.data.status) {
-        case 'initiate':
+        case "initiate":
           setReady(false);
           break;
-        case 'ready':
+        case "ready":
           setReady(true);
           break;
-        case 'complete':
+        case "complete":
           // Cosine similarity search in pgvector
           const searchResults = await search(db.current, e.data.embedding);
           console.log({ searchResults });
@@ -63,11 +63,11 @@ export default function App() {
     };
 
     // Attach the callback function as an event listener.
-    worker.current.addEventListener('message', onMessageReceived);
+    worker.current.addEventListener("message", onMessageReceived);
 
     // Define a cleanup function for when the component is unmounted.
     return () =>
-      worker.current.removeEventListener('message', onMessageReceived);
+      worker.current.removeEventListener("message", onMessageReceived);
   });
 
   const classify = useCallback((text) => {
@@ -79,18 +79,18 @@ export default function App() {
     <main className="flex min-h-screen flex-col items-center justify-center p-12">
       <h1 className="text-5xl font-bold mb-2 text-center">Transformers.js</h1>
       <h2 className="text-2xl mb-4 text-center">
-        100% in-browser Semantic Search with{' '}
+        100% in-browser Semantic Search with{" "}
         <a
           className="underline"
           href="https://huggingface.co/docs/transformers.js"
         >
           Transformers.js
         </a>
-        {', '}
+        {", "}
         <a className="underline" href="https://github.com/electric-sql/pglite">
           PGlite
-        </a>{' '}
-        {' + '}
+        </a>{" "}
+        {" + "}
         <a className="underline" href="https://github.com/pgvector/pgvector">
           pgvector!
         </a>
@@ -126,7 +126,7 @@ export default function App() {
         <>
           <p className="text-center">Similarity Search results:</p>
           <pre className="bg-gray-100 p-2 rounded">
-            {!ready || !result ? 'Loading...' : JSON.stringify(result)}
+            {!ready || !result ? "Loading..." : JSON.stringify(result)}
           </pre>
         </>
       )}
