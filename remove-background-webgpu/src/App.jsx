b4802cfb70b5e4e555c64851f1c3e1a2c1465093
Joshua Lochner <admin@xenova.com>
2024-08-27 20:28:53 +0000

    Formatting

--- remove-background-webgpu/src/App.jsx
+++ remove-background-webgpu/src/App.jsx
@@ -118,26 +118,27 @@ export default function App() {
 
   const downloadAsZip = async () => {
     const zip = new JSZip();
-    const promises = images.map((image, i) =>
-      new Promise((resolve) => {
-        const canvas = document.createElement("canvas");
-        const ctx = canvas.getContext("2d");
+    const promises = images.map(
+      (image, i) =>
+        new Promise((resolve) => {
+          const canvas = document.createElement("canvas");
+          const ctx = canvas.getContext("2d");
 
-        const img = new Image();
-        img.src = processedImages[i] || image;
+          const img = new Image();
+          img.src = processedImages[i] || image;
 
-        img.onload = () => {
-          canvas.width = img.width;
-          canvas.height = img.height;
-          ctx.drawImage(img, 0, 0);
-          canvas.toBlob((blob) => {
-            if (blob) {
-              zip.file(`image-${i + 1}.png`, blob);
-            }
-            resolve(null);
-          }, "image/png");
-        };
-      })
+          img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+            canvas.toBlob((blob) => {
+              if (blob) {
+                zip.file(`image-${i + 1}.png`, blob);
+              }
+              resolve(null);
+            }, "image/png");
+          };
+        }),
     );
 
     await Promise.all(promises);
