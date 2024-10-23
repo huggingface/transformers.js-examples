c09b7d4193480fb85b16d2ebbe84d66649ae3360
Joshua Lochner <admin@xenova.com>
2024-10-23 03:17:32 +0000

    Set output to standalone

--- next-server/next.config.mjs
+++ next-server/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   // https://nextjs.org/docs/app/api-reference/next-config-js/serverExternalPackages
   serverExternalPackages: ["@huggingface/transformers"],
 };
