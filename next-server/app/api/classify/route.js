db1edb7d3268fdd1038a0450bfd2b0f7dfc77b77
Joshua Lochner <admin@xenova.com>
2024-10-25 05:47:18 +0000

    Update next-server example

--- next-server/app/api/classify/route.js
+++ next-server/app/api/classify/route.js
@@ -2,16 +2,18 @@
 
 import { pipeline } from "@huggingface/transformers";
 
-// NOTE: We attach the classifier to the global object to avoid loading it multiple times
+// NOTE: We attach the classifier to the global object to avoid unnecessary reloads during development
 const classifier = (globalThis.classifier ??= await pipeline(
   "text-classification",
   "Xenova/distilbert-base-uncased-finetuned-sst-2-english",
 ));
 
 export async function GET(request) {
-  // https://nextjs.org/docs/app/building-your-axpplication/routing/route-handlers#url-query-parameters
-  const searchParams = request.nextUrl.searchParams;
-  const text = searchParams.get("text");
+  const text = request.nextUrl.searchParams.get("text");
+
+  if (!text) {
+    return Response.json({ message: "No text provided" }, { status: 400 });
+  }
 
   const result = await classifier(text);
   return Response.json(result[0]);
