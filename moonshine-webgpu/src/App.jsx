cdea153d9c62125512cf6c9135529a4007ee2bba
Joshua Lochner <admin@xenova.com>
2024-12-17 21:05:09 +0000

    Improve UI on mobile

--- moonshine-webgpu/src/App.jsx
+++ moonshine-webgpu/src/App.jsx
@@ -2,9 +2,8 @@ import Scene from "./components/Scene";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-900">
-      <Scene className="w-full h-full" />
-    </div>
+      // https://github.com/tailwindlabs/tailwindcss/discussions/4515#discussioncomment-4182377
+      <Scene className="flex flex-col items-center justify-center w-screen supports-[height:100cqh]:h-[100cqh] supports-[height:100svh]:h-[100svh] bg-gray-900" />
   );
 }
 
