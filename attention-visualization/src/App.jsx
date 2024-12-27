afeeae1abf32b3f3a229b9f4f193e4142580333c
Joshua Lochner <admin@xenova.com>
2024-12-27 12:10:57 +0000

    Add multi-layer support

--- attention-visualization/src/App.jsx
+++ attention-visualization/src/App.jsx
@@ -11,6 +11,7 @@ const WIDTH = 2;
 const HEIGHT = 2;
 const WIDTH_SEGMENTS = 32;
 const HEIGHT_SEGMENTS = 32;
+const LAYER_SPACING = 1;
 const X_SPACING = 0.25;
 const Y_SPACING = HEIGHT - 0.25;
 const Z_SPACING = 1;
@@ -18,12 +19,15 @@ const HOVER_PADDING = Y_SPACING + 2;
 const ZOOM_DISTANCE = 3.5;
 
 const CAMERA_ANGLE = (Math.PI * 5) / 12;
-const CAMERA_DISTANCE = 12;
+const CAMERA_DISTANCE = 16;
 const DEFAULT_CAMERA_POSITION = [
   -CAMERA_DISTANCE * Math.cos(CAMERA_ANGLE),
   3.5,
   CAMERA_DISTANCE * Math.sin(CAMERA_ANGLE),
 ];
+
+const TRANSLATE_ZONE_WIDTH = 0.1; // 10%
+const TRANSLATE_SPEED = 4;
 
 function useColorTexture(color) {
   return useMemo(() => {
@@ -37,7 +41,8 @@ function useColorTexture(color) {
   }, [color]);
 }
 
-const NUM_LAYERS = 12;
+const NUM_LAYERS = 12; // ADD THIS
+const NUM_HEADS = 6;
 
 const COLORS = [
   "#ff4444",
@@ -53,33 +58,50 @@ const COLORS = [
   "#ff7f0e",
   "#ffffff",
 ];
+const ATTENTION_DATA = Array.from(
+  { length: NUM_HEADS * NUM_LAYERS },
+  (_, i) => {
+    const [layerIndex, headIndex] = [Math.floor(i / NUM_HEADS), i % NUM_HEADS];
 
-const ATTENTION_DATA = Array.from({ length: NUM_LAYERS }, (_, i) => ({
-  color: COLORS[i % COLORS.length],
-  label: `Layer ${i + 1}`,
-  position: [
-    (i - NUM_LAYERS / 2 - 1) * X_SPACING - WIDTH / 2,
-    0.5 * HEIGHT - 1,
-    (i - NUM_LAYERS / 2) * Z_SPACING,
-  ],
-}));
+    // Base color for the layer
+    const baseColor = new THREE.Color(COLORS[layerIndex % COLORS.length]);
 
-function AttentionLayer({
+    // Slightly darken the color for each head
+    baseColor.offsetHSL(0, 0, -0.05 * headIndex);
+
+    const xOffset =
+      (layerIndex - NUM_LAYERS / 2) * (WIDTH + LAYER_SPACING + X_SPACING);
+    const zOffset = (headIndex - NUM_HEADS / 2) * Z_SPACING;
+    const data = {
+      color: `#${baseColor.getHexString()}`,
+      label: `Layer ${layerIndex + 1} Head ${headIndex + 1}`,
+      position: [
+        xOffset + (headIndex - NUM_HEADS / 2 - 1) * X_SPACING - WIDTH / 2,
+        0.5 * HEIGHT - 1,
+        zOffset + (headIndex - NUM_HEADS / 2) * Z_SPACING,
+      ],
+    };
+
+    return data;
+  },
+);
+
+function AttentionHead({
   position,
   rotation,
   color,
   text,
   index,
-  activeLayer,
-  setActiveLayer,
+  activeHead,
+  setActiveHead,
 }) {
   const groupRef = useRef();
   const texture = useColorTexture(color);
   const [hovered, setHovered] = useState(false);
 
   const active = useMemo(
-    () => hovered || activeLayer === index,
-    [hovered, activeLayer, index],
+    () => hovered || activeHead === index,
+    [hovered, activeHead, index],
   );
   const { positionY } = useSpring({
     positionY: active ? position[1] + Y_SPACING : position[1],
@@ -93,7 +115,7 @@ function AttentionLayer({
       : groupRef.current.traverse((o) => o.isMesh && o.layers.disable(1));
   }, [active]);
 
-  const visible = activeLayer === null || activeLayer === index;
+  const visible = activeHead === null || activeHead === index;
   return (
     <a.group
       ref={groupRef}
@@ -113,15 +135,14 @@ function AttentionLayer({
         setHovered(false);
       }}
       onClick={(e) => {
-        console.log("activeLayer === index", activeLayer === index);
-        if (activeLayer !== index) {
-          // Clicking on a different layer/background
-          setActiveLayer(null);
+        if (activeHead !== index) {
+          // Clicking on a different head/background
+          setActiveHead(null);
         }
         if (!visible) return;
 
         e.stopPropagation();
-        setActiveLayer(activeLayer === index ? null : index);
+        setActiveHead(activeHead === index ? null : index);
       }}
     >
       <Html
@@ -173,15 +194,15 @@ function AttentionLayer({
   );
 }
 
-function AttentionLayers({ activeLayer, setActiveLayer }) {
+function AttentionHeads({ activeHead, setActiveHead }) {
   return (
     <group position-y={1}>
       {ATTENTION_DATA.map((data, i) => (
-        <AttentionLayer
+        <AttentionHead
           key={i}
           index={i}
-          activeLayer={activeLayer}
-          setActiveLayer={setActiveLayer}
+          activeHead={activeHead}
+          setActiveHead={setActiveHead}
           position={data.position}
           rotation={[0, 0, 0]}
           color={data.color}
@@ -192,28 +213,60 @@ function AttentionLayers({ activeLayer, setActiveLayer }) {
   );
 }
 
-function CameraAnimator({ activeLayer }) {
+function CameraAnimator({ activeHead, mouseActive, mousePosition }) {
   const { camera } = useThree();
-  console.log({ activeLayer });
-
+  const [sceneCenter, setSceneCenter] = useState([0, 0, 0]);
   const center =
-    activeLayer !== null
-      ? ATTENTION_DATA[activeLayer].position.slice()
+    activeHead !== null
+      ? ATTENTION_DATA[activeHead].position.slice()
       : [0, 0, 0];
+
   center[1] += Y_SPACING;
   let targetPosition;
-  if (activeLayer !== null) {
+  if (activeHead !== null) {
     center[1] += 1;
     targetPosition = center.slice();
     targetPosition[2] += ZOOM_DISTANCE;
   } else {
-    targetPosition = DEFAULT_CAMERA_POSITION;
+    targetPosition = DEFAULT_CAMERA_POSITION.slice();
+
+    for (let i = 0; i < 3; i++) {
+      center[i] += sceneCenter[i];
+      targetPosition[i] += sceneCenter[i];
+    }
   }
+
+  useFrame((state, delta) => {
+    if (!mouseActive) return;
+    if (mousePosition.x < TRANSLATE_ZONE_WIDTH) {
+      setSceneCenter((prev) => {
+        const val = [...prev];
+        val[0] = Math.max(
+          val[0] - TRANSLATE_SPEED * delta,
+          ATTENTION_DATA.at(0).position[0],
+        );
+        return val;
+      });
+    } else if (mousePosition.x > 1 - TRANSLATE_ZONE_WIDTH) {
+      setSceneCenter((prev) => {
+        const val = [...prev];
+        val[0] = Math.min(
+          val[0] + TRANSLATE_SPEED * delta,
+          ATTENTION_DATA.at(-1).position[0],
+        );
+        return val;
+      });
+    }
+  });
+
   const spring = useSpring({
     pos: targetPosition,
     config: { mass: 1, tension: 280, friction: 60 },
   });
-  targetPosition = new THREE.Vector3(...targetPosition);
+  const springLookAt = useSpring({
+    lookAt: center,
+    config: { mass: 1, tension: 280, friction: 60 },
+  });
 
   const targetLookAt = useRef(new THREE.Vector3(0, 0, 0));
 
@@ -222,26 +275,56 @@ function CameraAnimator({ activeLayer }) {
       new THREE.Vector3(...spring.pos.get()),
       TRANSITION_ALPHA,
     );
-    const desiredLookAt = new THREE.Vector3(...center);
-    targetLookAt.current.lerp(desiredLookAt, TRANSITION_ALPHA);
+    targetLookAt.current.lerp(
+      new THREE.Vector3(...springLookAt.lookAt.get()),
+      TRANSITION_ALPHA,
+    );
     camera.lookAt(targetLookAt.current);
   });
   return null;
 }
 
 function AttentionVisualization() {
-  const [activeLayer, setActiveLayer] = useState(null);
+  const [activeHead, setActiveHead] = useState(null);
+  const [mousePosition, setMousePosition] = useState({ x: 0.5, y: 0.5 });
+  const [mouseActive, setMouseActive] = useState(false);
+
+  useEffect(() => {
+    // Added event listener
+
+    const handleMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      setMousePosition({
+        x: clientX / width,
+        y: clientY / height,
+        active: true,
+      });
+    };
+    const handleMouseLeave = () => setMouseActive(false);
+    const handleMouseEnter = () => setMouseActive(true);
+    document.addEventListener("mouseleave", handleMouseLeave);
+    document.addEventListener("mouseenter", handleMouseEnter);
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseleave", handleMouseLeave);
+      window.removeEventListener("mouseenter", handleMouseEnter);
+    };
+  }, []);
 
   return (
     <Canvas camera={{ fov: 45 }} gl={{ antialias: true }}>
-      <CameraAnimator activeLayer={activeLayer} />
+      <CameraAnimator
+        activeHead={activeHead}
+        mouseActive={mouseActive}
+        mousePosition={mousePosition}
+      />
       <color attach="background" args={["#040b1b"]} />
       <gridHelper args={[100, 100, "white", "gray"]} />
       <Suspense fallback={null}>
-        <AttentionLayers
-          activeLayer={activeLayer}
-          setActiveLayer={setActiveLayer}
-        />
+        <AttentionHeads activeHead={activeHead} setActiveHead={setActiveHead} />
         <Environment preset="forest" />
         <EffectComposer>
           <Bloom
