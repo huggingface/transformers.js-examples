89d31dd24902139cb4affb5b9199b29bdeda0e2c
Joshua Lochner <admin@xenova.com>
2024-12-31 15:44:35 +0000

    Update vite config

--- attention-visualization/vite.config.js
+++ attention-visualization/vite.config.js
@@ -5,4 +5,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: { target: "esnext" },
+  worker: { format: "es" },
 });
