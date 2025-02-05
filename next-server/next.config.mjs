5b6e0c18677e3e22ef42779a766f48e2ed0a4b18
Joshua Lochner <admin@xenova.com>
2025-02-05 16:32:53 +0200

    Set output mode to "standalone" (#41)

--- next-server/next.config.mjs
+++ next-server/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // https://nextjs.org/docs/app/api-reference/next-config-js/serverExternalPackages
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/output
+  output: "standalone",
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/serverExternalPackages
   serverExternalPackages: ["@huggingface/transformers"],
 };
 
