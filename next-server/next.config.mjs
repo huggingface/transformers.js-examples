f07199a8ce0d9a527d628aaa56977a81f65bd286
Joshua Lochner <admin@xenova.com>
2025-02-05 13:54:36 +0000

    Update next-server demo

--- next-server/next.config.mjs
+++ next-server/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   // https://nextjs.org/docs/app/api-reference/next-config-js/serverExternalPackages
   serverExternalPackages: ["@huggingface/transformers"],
 };
