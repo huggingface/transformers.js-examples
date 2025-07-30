7132afeeded586fbad6b5d0fe0dd1068b32ca9ca
Joshua Lochner <admin@xenova.com>
2025-07-30 18:40:15 -0400

    Add react translator source code (#65)
    * Add react translator source code
    
    * Delete vite.svg

---
+++ react-translator/src/components/Progress.jsx
@@ -0,0 +1,10 @@
+export default function Progress({ text, percentage }) {
+  percentage = percentage ?? 0;
+  return (
+    <div className="progress-container">
+      <div className="progress-bar" style={{ width: `${percentage}%` }}>
+        {text} ({`${percentage.toFixed(2)}%`})
+      </div>
+    </div>
+  );
+}
