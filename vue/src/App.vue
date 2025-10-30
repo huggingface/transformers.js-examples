cbbf68d12f10d3e9a5cd6a138f22da79664b025b
Sean <sjgallen@gmail.com>
2025-10-29 23:17:58 -0500

    initial commit for vue

---
+++ vue/src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
