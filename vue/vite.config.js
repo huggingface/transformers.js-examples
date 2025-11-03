b145b602c52898b1915c5c194f93df90e879999f
Sean <sjgallen@gmail.com>
2025-11-02 19:51:03 -0600

    ran format

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -6,13 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
